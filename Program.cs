f2f6eb736cf51e04413b42846a71e133aae5d93d
Aleksandr <aleks-sv@tut.by>
2022-11-29 22:26:16 +0300

    Create string array and PrintArray function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
+
+PrintArray(array);
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"'{array[i]}', ");
+        else Console.Write($"'{array[i]}'");
+    }
+    Console.WriteLine("]");
+}
