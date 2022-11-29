a18e3ede28b18b9907059d8795cf6b24f9e2850b
Aleksandr <aleks-sv@tut.by>
2022-11-29 23:46:37 +0300

    Create functiot to count elements satisfy condition

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
 ﻿string[] array = new string[] { "hello", "2", "world", ":-)" };
 
 PrintArray(array);
+int countElementArray = FindResultArray(array);
+Console.WriteLine($"{countElementArray}");
+
+int FindResultArray(string[] array)
+{
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        char[] arrayChar = array[i].ToCharArray();
+        if (arrayChar.Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 void PrintArray(string[] array)
 {
